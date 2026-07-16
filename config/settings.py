c4ee75fe3a6985b745b52d8b9ec2009d3ad56219
darshan2006-coder <drdarshan2006@gmail.com>
2026-07-16 12:46:57 +0530

    Phase 2: Configure PostgreSQL and Django backend

--- config/settings.py
+++ config/settings.py
@@ -80,9 +80,13 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "wonderindia_db",
+        "USER": "postgres",
+        "PASSWORD": "postgres123",
+        "HOST": "localhost",
+        "PORT": "5432",
     }
 }
 
