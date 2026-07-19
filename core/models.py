62062732a20d740c1ad6ba238fefb7a528c9577f
darshan2006-coder <drdarshan2006@gmail.com>
2026-07-19 12:01:16 +0530

    Phase 3: Complete Destination model and admin setup

--- core/models.py
+++ core/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Destination(models.Model):
+    name = models.CharField(max_length=100)
+    location = models.CharField(max_length=100)
+    rating = models.DecimalField(max_digits=2, decimal_places=1)
+    days = models.CharField(max_length=20)
+    price = models.PositiveIntegerField()
+    image = models.ImageField(upload_to="destinations/")
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
